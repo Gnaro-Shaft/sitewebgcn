9b4417ce8052295f9619fdee0592e861409f4318
Gnaro-Shaft <gced.nisus@gmail.com>
2026-07-26 21:44:55 +0200

    fix: add missing User and EmailService imports in articleController

--- server/controllers/articleController.js
+++ server/controllers/articleController.js
@@ -1,3 +1,5 @@
+const User = require("../models/User");
+const EmailService = require("../services/EmailService");
 const Article = require('../models/Article');
 const asyncHandler = require('../middleware/asyncHandler');
 
